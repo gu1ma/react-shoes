aabe82d6a1e07cc52090704d9baf5800f6f18aed
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-11-29 17:16:29 -0300

    [Add] Estilizacao da header

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,11 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 
-export default function Routes(){
+export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={Home}/>
+      <Route path="/" exact component={Home} />
       <Route path="/cart" component={Cart} />
     </Switch>
-  )
+  );
 }
