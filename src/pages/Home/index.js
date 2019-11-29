10887c0733b3a1eddbd4101707714b0e9685bce6
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-11-29 18:00:23 -0300

    [Add] Estilizacao da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT_ieRbHEYUkIK8u701u97xiPPwWPe2iqEj7JoIXvbVLPmjgXlWDR2x-GMS7CuaSKRFrqXU6jNI98xiKuxLNvXze5sJTwAJc_zyoktySaAQto7B-pqm0d_A&usqp=CAc"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+            3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT_ieRbHEYUkIK8u701u97xiPPwWPe2iqEj7JoIXvbVLPmjgXlWDR2x-GMS7CuaSKRFrqXU6jNI98xiKuxLNvXze5sJTwAJc_zyoktySaAQto7B-pqm0d_A&usqp=CAc"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+            3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT_ieRbHEYUkIK8u701u97xiPPwWPe2iqEj7JoIXvbVLPmjgXlWDR2x-GMS7CuaSKRFrqXU6jNI98xiKuxLNvXze5sJTwAJc_zyoktySaAQto7B-pqm0d_A&usqp=CAc"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+            3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
