649a72a2e57938a15252463bec5eafe840f7ed4f
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-12-05 15:24:32 -0300

    [Add] Buscando produtos da API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,62 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
 import { ProductList } from './styles';
 
 export default function Home() {
+  const [products, updateProducts] = useState([]);
+
+  async function fetchPosts() {
+    const { data } = await api.get('products');
+
+    const newData = data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    updateProducts(newData);
+    return data;
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <ProductList>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT_ieRbHEYUkIK8u701u97xiPPwWPe2iqEj7JoIXvbVLPmjgXlWDR2x-GMS7CuaSKRFrqXU6jNI98xiKuxLNvXze5sJTwAJc_zyoktySaAQto7B-pqm0d_A&usqp=CAc"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
+      {products.map((product) => (
+        <li key={product.id}>
+          <img
+            src={product.image}
+            alt={product.title}
+          />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-            3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT_ieRbHEYUkIK8u701u97xiPPwWPe2iqEj7JoIXvbVLPmjgXlWDR2x-GMS7CuaSKRFrqXU6jNI98xiKuxLNvXze5sJTwAJc_zyoktySaAQto7B-pqm0d_A&usqp=CAc"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-            3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT_ieRbHEYUkIK8u701u97xiPPwWPe2iqEj7JoIXvbVLPmjgXlWDR2x-GMS7CuaSKRFrqXU6jNI98xiKuxLNvXze5sJTwAJc_zyoktySaAQto7B-pqm0d_A&usqp=CAc"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-            3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" />
+                3
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
 }
